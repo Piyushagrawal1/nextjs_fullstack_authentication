5e655e4f20ecc95937ed9ccfd37b54eaa00fb40a
Piyushagrawal1 <agarwalpiyush166@gmail.com>
2024-03-24 15:19:23 +0530

    Update MongoDB URL and fix signup route

--- src/app/api/users/signup/route.ts
+++ src/app/api/users/signup/route.ts
@@ -1,51 +1,55 @@
 import { connectDB } from "@/dbConfig/dbConfig";
 import User from "@/models/userModel";
-import { NextResponse, NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import bcryptjs from "bcryptjs";
-import { sendEmail } from '@/helpers/mailer'
+import { sendEmail } from "@/helpers/mailer";
 
 
-connectDB();
-export async function POST(req: NextRequest) {
+connectDB()
+
+
+export async function POST(request: NextRequest) {
   try {
-    const reqBody = await req.json();
-    const { uername, email, Password } = reqBody;
-    //validation
+    const reqBody = await request.json()
+    const { username, email, password } = reqBody
+
     console.log(reqBody);
 
-    const user = await User.findOne({ email });
+    //check if user already exists
+    const user = await User.findOne({ email })
+
     if (user) {
       return NextResponse.json({ error: "User already exists" }, { status: 400 })
     }
 
-    const hashedPassword = await bcryptjs.hash(Password, 10);
+    //hash password
+    const salt = await bcryptjs.genSalt(10)
+    const hashedPassword = await bcryptjs.hash(password, salt)
 
     const newUser = new User({
-      uername,
+      username,
       email,
-      password: hashedPassword,
-    });
+      password: hashedPassword
+    })
 
-    const savedUser = await newUser.save();
-    console.log(savedUser)
+    const savedUser = await newUser.save()
+    console.log(savedUser);
 
-    //send varification main
-    await sendEmail({ email, emailType: 'VERIFY', userId: savedUser._id })
-    return NextResponse.json({ 
-      message: "User Registered successfully",
+    //send verification email
+
+    await sendEmail({ email, emailType: "VERIFY", userId: savedUser._id })
+
+    return NextResponse.json({
+      message: "User created successfully",
       success: true,
-      savedUser,
-    }, { status: 201 })
+      savedUser
+    })
+
+
+
 
   } catch (error: any) {
-    return NextResponse.json(
-      {
-        error: error.message,
-      },
-      {
-        status: 500,
-      }
-    );
+    return NextResponse.json({ error: error.message }, { status: 500 })
+
   }
 }
-
